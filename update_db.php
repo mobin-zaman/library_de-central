9eb9acc1efc34903ab2c1cf56509762fdf7bbf36
mobin-zaman <38745180+mobin-zaman@users.noreply.github.com>
2018-11-01 18:57:21 +0600

    mail issue

--- update_db.php
+++ update_db.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['u_id'])){
     header("Location: index.php");
     exit;
   }
-  $conn = new mysqli('localhost','root','amarsql','library');
+  $conn=new mysqli('localhost','sadatjub_book','*BFQK^QQRIi;','sadatjub_lib');
       if($conn->connect_error) die("connection to db failed");
 
     if($_SERVER['REQUEST_METHOD']=='POST'){
@@ -25,10 +25,20 @@ if(!isset($_SESSION['u_id'])){
         }
         //else update the db
         else {
-            $conn = new mysqli('localhost','root','amarsql','library');
+            $conn=new mysqli('localhost','sadatjub_book','*BFQK^QQRIi;','sadatjub_lib');
                         if($conn->connect_error) die("connection to db failed");
             $sql="INSERT INTO `book` (`b_id`, `title`, `author`, `isbn`, `u_id`, `category`) VALUES (NULL, '$title', '$author', '$isbn', '$u_id', '$category')";
             $conn->query($sql);
+            $sql="SELECT username FROM user WHERE u_id='$u_id'";
+            $username=$conn->query($sql);
+            $sql = "SELECT email FROM user WHERE u_id!='$u_id'"; //u_id!='u_id' needed
+            $result=$conn->query($sql);
+
+            while($row=$result->fetch_assoc()){
+                $msg=$title+" written by "+$author+" has been added to database"+"<br>"+"It can be issued from "+$username;
+                mail($row['email'],"New book has been added!",$msg);
+            }
+
             echo "<script> location.href='userHome.php'; </script>"; 
         }
 
